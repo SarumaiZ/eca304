d1b159b25f73f863ca314251ccc320806397de15
Matheus Silva <matheuszaitune@hotmail.com>
2022-04-18 18:33:05 -0300

    Herança de classes

--- Atuador_Heranca.py
+++ Atuador_Heranca.py
@@ -1,3 +1,6 @@
+# Matheus Gonçalves Zaitune da Silva RA: 19.01247-0
+# Gabriel Semitan RA: 19.00961-5
+
 class Dispositivo():
     def __init__(self, nome):
         self.nome = nome
@@ -6,43 +9,72 @@ class Dispositivo():
         return self.nome
 
 
-class Sensor(Dispositivo):
-    def __init__(self, nome, dado=None):
+class Atuador(Dispositivo):
+    def __init__(self, nome, estado):
         super().__init__(nome)
-        self.dado = dado
+        if estado >= 1:
+            self.estado = 1
+        if estado <= 0:
+            self.estado = 0
 
-    def enviaDado(self):
-        from random import randrange
-        self.dado = randrange(1023)
-        return self.dado
-
-
-class Microcontrolador(Dispositivo):
-    def __init__(self, nome):
-        super().__init__(nome)
-        self.dadosSensores = dict()
-        self.dadosAtuadores = dict()
-
-    def recebeDado(self, nomeSensor, valorSensor):
-        self.dadosSensores[nomeSensor] = valorSensor
-
-    def enviaDado(self, nomeAtuador, estado):
-        self.dadosAtuadores[nomeAtuador] = estado
-        return self.dadosAtuadores[nomeAtuador]
-
-    def exibeInfo(self, tipo, nomeDispositivo):
-        if tipo == "sensor":
-            print("{0}: {1}".format(nomeDispositivo,
-                  self.dadosSensores[nomeDispositivo]))
-        if tipo == "atuador":
-            print("{0}: {1}".format(nomeDispositivo,
-                  self.dadosAtuadores[nomeDispositivo]))
+    def exibeEstado(self):
+        if self.estado == 0:
+            print("Nome: {0} Estado: Desligado".format(self.nome))
+        else:
+            print("Nome: {0} Estado: Ligado".format(self.nome))
 
 
-sensor1 = Sensor("Ultrassom")
-arduino = Microcontrolador("Arduino")
-arduino.recebeDado(sensor1.retornaID(), sensor1.enviaDado())
-arduino.exibeInfo("sensor", sensor1.retornaID())
-sensor2 = Sensor("Sensor de Linha")
-arduino.recebeDado(sensor2.retornaID(), sensor2.enviaDado())
-arduino.exibeInfo("sensor", sensor2.retornaID())
+class LED(Atuador):
+    def __init__(self, nome, estado):
+        super().__init__(nome, estado)
+
+    def liga(self):
+        self.estado = 1
+
+    def desliga(self):
+        self.estado = 0
+
+
+class motor(Atuador):
+    def __init__(self, nome, estado):
+        super().__init__(nome, estado)
+
+    def atribuiDirecao(self, dir):
+        self.direcao = dir
+
+    def atribuiVelocidade(self, vel):
+        if vel > 255:
+            self.velocidade = 255
+            self.estado = 1
+        elif vel <= 0:
+            self.velocidade = 0
+            self.estado = 0
+        else:
+            self.velocidade = vel
+            self.estado = 1
+
+    def exibeEstado(self):
+        if self.estado == 0:
+            print("Nome: {0} Estado: Desligado".format(self.nome))
+        else:
+            if self.direcao == 0:
+                print("Nome: {0} Estado: Ligado Direcao: Anti-Horário Velocidade: {1}".format(
+                    self.nome, self.velocidade))
+            else:
+                print("Nome: {0} Estado: Ligado Direcao: Horário Velocidade: {1}".format(
+                    self.nome, self.velocidade))
+
+
+M1 = motor("Máquina do Prazer", 0)
+L1 = LED("Lampada", 4)
+
+L1.exibeEstado()
+L1.desliga()
+L1.exibeEstado()
+
+M1.exibeEstado()
+M1.atribuiVelocidade(100)
+M1.atribuiDirecao(0)
+M1.exibeEstado()
+M1.atribuiVelocidade(300)
+M1.exibeEstado()
